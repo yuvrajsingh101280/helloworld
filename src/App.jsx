a1e6a3b7664e651e6529a969770b666da3c54e51
yuvrajsingh101280 <singhyuvraj65035@gmail.com>
2024-10-20 10:39:56 +0530

    folder structure complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import Grid from "./components/grid/Grid";
 
 const App = () => {
   return (
     <div>
-      
+      <Grid />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/sqaure/Square.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Square = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Square

017b93910664cc4cad3e569b812d97daa4c9f4c9
Nikhil Kumar <kumarnikhil48578@gmail.com>
2024-10-20 10:59:03 +0530

    helo

--- src/components/sqaure/Square.jsx
+++ src/components/sqaure/Square.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+// src/components/Square.js
+import React from 'react';
 
-const Square = () => {
+const Square = ({ particles, owner, onClick }) => {
+  const color = owner === 1 ? 'red' : owner === 2 ? 'blue' : 'grey';
+
   return (
-    <div>
-      
+    <div
+      onClick={onClick}
+      style={{
+        width: '80px',
+        height: '80px',
+        backgroundColor: color,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: '24px',
+        border: '1px solid black',
+      }}
+    >
+      {particles}
     </div>
-  )
-}
+  );
+};
 
-export default Square
+export default Square;
+
